be886a0bc9504647d5ab5fe2ad2ea51115cc21fe
Kevin Gamaliel <gamagambogam@gmail.com>
2022-03-17 22:55:12 +0700

    First commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,13 @@
-<button class="btn">Hello daisyUI</button>
+<script>
+
+</script>
+
+
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md ">
+        <h1 class="text-5xl font-bold mb-6">Hello World</h1>
+        <class class="py-6">Welcome to my Sveltekit project starter. Happy hacking!</class>       
+      </div>
+    </div>
+</div> 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,17 @@
 module.exports = {
   content: ['./src/routes/**/*.{svelte,js,ts}'],
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+        {
+            cyberpunk: {
+                ...require("daisyui/src/colors/themes")["[data-theme=cyberpunk]"],
+                "nautral-content": "#FFFFFF",
+                "base-100": "#FFFFFF",
+                "base-200": "#F9FAFB",
+                "base-300": "#D1D5DB"  
+            },    
+        },
+    ]
+  },
 };
